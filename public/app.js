55c5fc441dd68194496b139d86303101ea607df6
Adil Farooq <avfarooq@gmail.com>
2018-10-28 15:55:01 -0500

    Login working finally

---
+++ public/app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", event => {
+
+const app = firebase.app();
+console.log(app);
+
+});
+
+
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    
+    firebase.auth().signInWithPopup(provider)
+        
+            .then(result => {
+                const user = result.user;        
+            })  
+}
+
