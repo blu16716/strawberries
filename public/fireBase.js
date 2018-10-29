332823ff907dfd34716d3ffca048544a3436808a
Adil Farooq <avfarooq@gmail.com>
2018-10-28 22:42:53 -0500

    Almost there

--- public/fireBase.js
+++ public/fireBase.js
@@ -1,15 +1,16 @@
-var app_fireBase = {}
-(function(){
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyC2W3RL8Tzpge2S-kcSacYHUjNSpGsbF_o",
-    authDomain: "strawberries-93286.firebaseapp.com",
-    databaseURL: "https://strawberries-93286.firebaseio.com",
-    projectId: "strawberries-93286",
-    storageBucket: "strawberries-93286.appspot.com",
-    messagingSenderId: "667344337550"
+var fireBase = fireBase || firebase;
+var hasInit = false;
+var config = {
+  apiKey: "AIzaSyC2W3RL8Tzpge2S-kcSacYHUjNSpGsbF_o",
+  authDomain: "strawberries-93286.firebaseapp.com",
+  databaseURL: "https://strawberries-93286.firebaseio.com",
+  projectId: "strawberries-93286",
+  storageBucket: "strawberries-93286.appspot.com",
+  messagingSenderId: "667344337550"
   };
-  firebase.initializeApp(config);  
+if(!hasInit){
+    firebase.initializeApp(config);
+    hasInit = true;
+}
 
-  app_fireBase = firebase;
-})()
+
